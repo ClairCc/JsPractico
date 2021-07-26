8f0119a136a064161e207d6de000f6131ea79c63
clair <cavanzoariasc@gmail.com>
2021-07-26 11:14:23 -0500

    Calculando los porcentajes

---
+++ source/descuentos.js
@@ -0,0 +1,28 @@
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = descuento * 0.01;
+  const precioConDescuento = precio - precio * porcentajePrecioConDescuento;
+  return precioConDescuento;
+}
+const cupon = ["Todo2020", "ConToda", "Prime"];
+function PriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+  const inputDiscount = document.getElementById("InputDiscount");
+  let discountValue = inputDiscount.value;
+  const inputCupon = document.getElementById("Cupon");
+  const cuponValue = inputCupon.value;
+  switch (cuponValue) {
+    case "Todo2020":
+      discountValue = discountValue * 2;
+      break;
+    default:
+      console.log("Hola");
+  }
+  const precioCondescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = `El precio con descuento son: $${precioCondescuento}`;
+}
