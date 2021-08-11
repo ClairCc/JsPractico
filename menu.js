8d572914026df638a480dd40216a9a5871cfdaae
clair <cavanzoariasc@gmail.com>
2021-08-11 07:50:43 -0500

    Realizando las correciones del header para que sea responsive

--- menu.js
+++ menu.js
@@ -1,10 +1,13 @@
 const all = document.getElementById("all");
+const main = document.getElementById("main");
+const espacio = document.getElementById("espacio");
 function headerTablets() {
   all.style.display = "block";
-  const main = document.getElementById("main");
   main.style.display = "none";
+  espacio.style.display = "none";
 }
 function offMenu() {
   all.style.display = "none";
   main.style.display = "inherit";
+  espacio.style.display = "inherit";
 }
